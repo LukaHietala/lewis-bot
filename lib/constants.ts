ecbfffee700c3e6c0df70b897e93fb80545a2777
Thomas <tmsnijder2002@gmail.com>
2022-06-06 16:37:06 +0200

    fixed the constants, and var to let

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,18 +3,15 @@ import {
     BaseChannels,
     BaseErrors,
     BaseColors,
-    BaseGuilds,
 } from './base';
 
 const Emojis: typeof BaseEmojis = BaseEmojis;
 const Channels: typeof BaseChannels = BaseChannels;
 const Errors: typeof BaseErrors = BaseErrors;
 const Colors: typeof BaseColors = BaseColors;
-const Guilds: typeof BaseGuilds = BaseGuilds;
 export class Constants {
     static Emojis: typeof BaseEmojis = Emojis;
     static Channels: typeof BaseChannels = Channels;
     static Errors: typeof BaseErrors = Errors;
     static Colors: typeof BaseColors = Colors;
-    static Guilds: typeof BaseGuilds = Guilds;
 }
