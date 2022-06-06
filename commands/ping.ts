0f805a070dd8a856f49ed6baa0a373e85237c3e8
D3FUZED <tmsnijder2002@gmail.com>
2022-06-06 22:35:04 +0200

    edited the ping command to include latency

--- commands/ping.ts
+++ commands/ping.ts
@@ -5,8 +5,13 @@ import { Constants } from '../lib/constants';
 export = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Replies with the bot and API ping!'),
     async execute(interaction: any, client: Client) {
-        interaction.reply({ content: ' Pong!', ephemeral: true });
+
+        const msg = await interaction.channel.send('Done!');
+
+        const latency = msg.createdTimestamp - interaction.createdTimestamp;
+
+        await interaction.reply({ content: `**Bot Latency**: \`${latency}ms\`, **API Latency**: \`${Math.round(client.ws.ping)}ms\``, ephemeral: false });
     },
 };
