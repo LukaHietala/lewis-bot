46388291386acbc101cb9bb5b0cd73f47a26c3bf
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2022-06-07 18:15:15 +0300

    Made the ping command ephemeral.

--- commands/ping.ts
+++ commands/ping.ts
@@ -8,10 +8,10 @@ export = {
         .setDescription('Replies with Pong!'),
     async execute(interaction: any, client: Client) {
 
-        const msg = await interaction.reply({content: 'Pinging...', ephemeral: false });
+        const msg = await interaction.reply({content: 'Pinging...', ephemeral: true });
 
         const latency = msg.createdTimestamp - interaction.createdTimestamp;
 
-        await interaction.editReply({ content: `**Bot Latency**: \`${latency}ms\`, **API Latency**: \`${Math.round(client.ws.ping)}ms\``, ephemeral: false });
+        await interaction.editReply({ content: `**Bot Latency**: \`${latency}ms\`, **API Latency**: \`${Math.round(client.ws.ping)}ms\``, ephemeral: true });
     },
 };
