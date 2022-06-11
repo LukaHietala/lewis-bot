0dbc9c59a4f623f72b7b62b9c12ec666179caa86
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2022-06-11 22:23:29 +0300

    Fixed dist

--- dist/deploy-commands.js
+++ dist/deploy-commands.js
@@ -1,23 +1,51 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator['throw'](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next(),
+            );
+        });
+    };
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.deployCommands = void 0;
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
-const rest_1 = require("@discordjs/rest");
-const v10_1 = require("discord-api-types/v10");
-const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require('fs'));
+const path_1 = __importDefault(require('path'));
+const rest_1 = require('@discordjs/rest');
+const v10_1 = require('discord-api-types/v10');
+const dotenv_1 = __importDefault(require('dotenv'));
 function deployCommands(clientId, guildId, token) {
     return __awaiter(this, void 0, void 0, function* () {
         //Start refreshing the application commands.
@@ -26,7 +54,7 @@ function deployCommands(clientId, guildId, token) {
         const commandsPath = path_1.default.join(__dirname, 'commands');
         const commandFiles = fs_1.default
             .readdirSync(commandsPath)
-            .filter((file) => file.endsWith('.ts'));
+            .filter((file) => file.endsWith('.js'));
         dotenv_1.default.config();
         //Push the commands to Discord API.
         for (const file of commandFiles) {
@@ -39,9 +67,11 @@ function deployCommands(clientId, guildId, token) {
         yield rest.setToken(token);
         yield rest
             .put(v10_1.Routes.applicationGuildCommands(clientId, guildId), {
-            body: commands,
-        })
-            .then(() => console.log('Successfully registered application commands.'))
+                body: commands,
+            })
+            .then(() =>
+                console.log('Successfully registered application commands.'),
+            )
             .catch(console.error);
     });
 }
