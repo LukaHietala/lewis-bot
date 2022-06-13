7c5198d27f2a84a884ea16bbdd4dcd8cbac674ca
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2022-06-13 23:07:04 +0300

    Added user info.

--- commands/info.ts
+++ commands/info.ts
@@ -1,5 +1,10 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import {
+    CommandInteraction,
+    MessageActionRow,
+    MessageButton,
+    UserResolvable,
+} from 'discord.js';
 import { MessageEmbed } from 'discord.js';
 import { Constants } from '../lib/constants';
 
@@ -30,33 +35,140 @@ export = {
         switch (command) {
             case 'user':
                 const user = interaction.options.getUser('user');
+                const member = await interaction.guild!.members.fetch(
+                    user as UserResolvable,
+                );
+                let avatarUrl: string = `${user!.avatarURL({ format: 'png' })}`;
+                const row = new MessageActionRow().addComponents(
+                    new MessageButton()
+                        .setLabel('Avatar (png)')
+                        .setURL(avatarUrl)
+                        .setStyle('LINK'),
+                );
+
+                const roles = member.roles.cache
+                    .filter((roles) => roles.id !== interaction.guild!.id)
+                    .map((role) => role.toString());
+
+                const uselessWords = [
+                    'Connect, ',
+                    'Create Instant Invite, ',
+                    'Add Reactions, ',
+                    'View Audit Log, ',
+                    'Priority Speaker, ',
+                    'Stream, ',
+                    'View Channel, ',
+                    'Send TTS Messages, ',
+                    'Embed Links, ',
+                    'Attach Files, ',
+                    'Read Message History, ',
+                    'Use External Emojis, ',
+                    'Use Voice Activity, ',
+                    'Mute Members, ',
+                    'Deafen Members, ',
+                    'Move Members, ',
+                    'Change Nickname, ',
+                    'Use Vad, ',
+                    'Use Application Commands, ',
+                    'Request To Speak, ',
+                    'Speak, ',
+                    'Use Public Threads, ',
+                    'Create Public Threads, ',
+                    'Use Private Threads, ',
+                    'Create Private Threads, ',
+                    'Use External Stickers, ',
+                    'Send Messages In Threads, ',
+                    'Start Embedded Activities, ',
+                ];
+
+                var expStr = uselessWords.join('\\b|\\b');
                 const embed = new MessageEmbed()
                     .setColor(Constants.Colors.DEFAULT)
                     .setAuthor({
                         name: `Information about ${user?.tag}`,
                         iconURL: user?.displayAvatarURL(),
                     })
+                    .setThumbnail(user!.displayAvatarURL())
                     .addFields(
                         {
                             // @ts-ignore
-                            name: 'User ID',
-                            value: user?.id,
-                            inline: false,
-                        },
-                        {
-                            name: 'User Tag',
-                            value: user?.tag,
-                            inline: false,
+                            name: 'Joined',
+                            value: `<t:${member.joinedTimestamp}:F>`,
+                            inline: true,
                         },
                         {
                             name: 'User Created At',
-                            value: `<t:${user!.createdTimestamp}>`,
+                            value: `<t:${user!.createdTimestamp}:F>`,
+                            inline: true,
+                        },
+                        {
+                            name: `Roles (${member?.roles.cache.size - 1})`,
+                            value: roles.toString() || 'None',
+                            inline: false,
+                        },
+                        {
+                            name: "Member's key permissions",
+                            value:
+                                member?.permissions
+                                    .toArray()
+                                    .join(', ')
+                                    .toString()
+                                    .toLowerCase()
+                                    .replaceAll('_', ' ')
+                                    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+                                        letter.toUpperCase(),
+                                    )
+                                    .trim()
+                                    .replace(new RegExp(expStr, 'gi'), '')
+                                    .replace(/ +/g, ' ') || 'None',
+                            inline: false,
+                        },
+                        {
+                            name: 'Acknowledgements',
+                            value: `None.`,
+                            inline: false,
+                        },
+                        {
+                            name: 'Server nickname',
+                            value: member?.nickname || 'None.',
+                            inline: false,
+                        },
+                    )
+                    .setFooter(`User ID: ${user?.id}`);
+                interaction.reply({
+                    embeds: [embed],
+                    ephemeral: true,
+                    components: [row],
+                });
+                break;
+            case 'server':
+                const server = interaction.guild;
+                const embed2 = new MessageEmbed()
+                    .setColor(Constants.Colors.DEFAULT)
+                    .setAuthor({
+                        name: `Information about ${server!.name}`,
+                        iconURL: server!.iconURL()?.toString(),
+                    })
+                    .setDescription(
+                        `Server ID: ${server!.id}\nServer's Owner: <@${
+                            server!.ownerId
+                        }>\nModerators / Admins: Coming soon!`,
+                    )
+                    .addFields(
+                        {
+                            // @ts-ignore //Same issues as before...
+                            name: 'Server Created At',
+                            value: `<t:${server!.createdTimestamp}:F>`,
+                            inline: false,
+                        },
+                        {
+                            name: 'Member Count',
+                            value: server!.memberCount,
                             inline: false,
                         },
                     );
-                interaction.reply({ embeds: [embed], ephemeral: true });
-                break;
-            case 'server':
+
+                interaction.reply({ embeds: [embed2], ephemeral: true });
                 break;
             default:
                 interaction.reply({
