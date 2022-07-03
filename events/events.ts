44bd3d2fbf854e0109bfcdcbbffc74bb0557c671
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2022-07-03 15:51:24 +0300

    Removed welcoming message

--- events/events.ts
+++ events/events.ts
@@ -1,8 +1,6 @@
 import { Client } from 'discord.js';
 import ready from './ready';
-import guildMemberAdd from './guildMemberAdd';
 
 export default (client: Client): void => {
     ready(client);
-    guildMemberAdd(client);
 };
